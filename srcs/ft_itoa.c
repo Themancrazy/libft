a6ca553652921338120708a1dcaca2a70c3f3781
anjansse <ajanssen@live.fr>
2023-03-09 22:52:35 +0000

    fix: resolve wrong syntax error

--- srcs/ft_itoa.c
+++ srcs/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: anjansse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 22:06:54 by anjansse          #+#    #+#             */
-/*   Updated: 2018/12/03 21:10:18 by anjansse         ###   ########.fr       */
+/*   Updated: 2023/03/09 20:08:30 by anjansse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_itoa(int n)
 	while (sign /= 10)
 		count++;
 	sign = n < 0 ? 1 : 0;
-	count = n < 0 ? count += 1 : count;
+	count = n < 0 ? count + 1 : count;
 	if (n == -2147483648)
 		return (str = ft_strdup("-2147483648"));
 	str = ft_strnew(count);
@@ -31,7 +31,7 @@ char	*ft_itoa(int n)
 		return (NULL);
 	if (sign == 1)
 		str[0] = '-';
-	n = n < 0 ? n *= -1 : n;
+	n = n < 0 ? n * -1 : n;
 	while (--count >= sign)
 	{
 		str[count] = n >= 10 ? (n % 10) + 48 : n + 48;
--- srcs/printf/ft_iitoa.c
+++ srcs/printf/ft_iitoa.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   ft_iitoa.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anjansse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 22:06:54 by anjansse          #+#    #+#             */
-/*   Updated: 2019/02/26 11:22:10 by anjansse         ###   ########.fr       */
+/*   Updated: 2023/03/09 22:50:26 by anjansse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ char			*ft_iitoa(int64_t n)
 	while (sign /= 10)
 		count++;
 	sign = n < 0 ? 1 : 0;
-	count = n < 0 ? count += 1 : count;
+	count = n < 0 ? count + 1 : count;
 	str = ft_strnew(count);
 	if (!str)
 		return (NULL);
 	if (sign == 1)
 		str[0] = '-';
-	n = n < 0 ? n *= -1 : n;
+	n = n < 0 ? n * -1 : n;
 	while (--count >= sign)
 	{
 		str[count] = n >= 10 ? (n % 10) + 48 : n + 48;
